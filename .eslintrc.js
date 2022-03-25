08acbed17db93befd5d42e5226d6c8e2f04480b6
Nantanart Takonuan <nantanart@six.network>
2022-03-25 12:06:18 +0700

    add comma dangle only-multiline

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,8 @@ module.exports = {
     ],
     'arrow-parens': ['error', 'as-needed'],
     'vue/multiline-html-element-content-newline': 'off',
-    'comma-dangle': ['error', 'never']
+    'comma-dangle': ['error', 'never'],
+    "comma-dangle": "off",
+    "@typescript-eslint/comma-dangle": ["error", "only-multiline"]
   }
 };
